7625deadc747bf2496098f85c3157671413b930e
Itamar1m <itamarym@gmail.com>
2021-04-11 18:22:57 +0300

    h

--- trade_tracks/settings.py
+++ trade_tracks/settings.py
@@ -80,31 +80,31 @@ WSGI_APPLICATION = 'trade_tracks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'trade_tracks2',
-
-        'USER': 'postgres',
-
-        'PASSWORD': 'postgres',
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
+# DATABASES = {
+
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#         'NAME': 'trade_tracks2',
+
+#         'USER': 'postgres',
+
+#         'PASSWORD': 'postgres',
+
+#         'HOST': 'localhost',
+
+#         'PORT': '5432',
+
+#     }
+
+# }
    
 
 # Password validation
